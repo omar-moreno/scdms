d1fd9a3bbd426f150be5f4eac921736cd0bcdd4f
Omar Moreno <email@omarmoreno.net>
2024-01-18 17:43:08 -0800

    Add ability to specify the depth of directories to crawl.

--- utils/file_crawler.py
+++ utils/file_crawler.py
@@ -2,21 +2,23 @@
 import os
 import pwd
 
-def main(path : str): 
+def main(path : str, depth : int = 1): 
    
     user_dict = {}
+    path_length = len(path) + len(os.path.sep)
+    for dpath, dnames, fnames in os.walk(path):
+        level = dpath[path_length:].count(os.path.sep)
+        if depth and level >= depth:
+            dnames.clear()
+        else: 
+            for dname in dnames: 
+                stats = os.stat(os.path.join(dpath, dname))
+                uid = stats.st_uid
+                user = pwd.getpwuid(uid)[0]
+                if user not in user_dict: 
+                    user_dict[user] = []
 
-    for root, dir_names, file_names in os.walk(path):
-        for dir_name in dir_names: 
-            dir_path = root+'/'+dir_name
-            stats = os.stat(dir_path)
-            uid = stats.st_uid
-            user = pwd.getpwuid(uid)[0]
-            if user not in user_dict: 
-                user_dict[user] = []
-
-            user_dict[user].append(dir_path)
-        break
+                user_dict[user].append(os.path.join(dpath, dname))
 
     with open('user_files.log', 'w') as f: 
         for key, value in user_dict.items():
@@ -30,7 +32,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-p', action='store', dest='path',
                         help='Path to directory to crawl.')
+    parser.add_argument('-d', action='store', dest='depth', 
+                        help='The directory depth to crawl.')
     args = parser.parse_args()
 
-    main(args.path)
+    main(args.path, int(args.depth))
 

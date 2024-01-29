fe0f3b4f1fa2373c104d30792c74126c862122a2
Omar Moreno <email@omarmoreno.net>
2024-01-29 11:44:40 -0800

    Move configuration to constructor. Require a config file to be specified. Require a file system path to be specified.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,8 +5,24 @@ from CDMSDataCatalog import CDMSDataCatalog
 
 class DCCrawler:
 
-    def __init__(self, dc : CDMSDataCatalog):
+    def __init__(self, dc : CDMSDataCatalog, config):
         self.dc = dc
+
+        if 'fs_prefix' not in config['crawler']: 
+            raise ValueError('Failed to provide dataset prefix on filesystem.')
+        self.fs_prefix = config['crawler']['fs_prefix']
+
+        self.dc_path = '/CDMS'
+        if 'dc_path' in config['crawler']:
+            self.dc_path = config['crawler']['dc_path']
+
+        self.resource_prefix = ''
+        if 'resource_prefix' in config['crawler']:
+            self.resource_prefix = config['crawler']['resource_prefix'])
+    
+        self.site = 'SLAC'
+        if 'site' in config['crawler']:
+            self.site = config['crawler']['site']
 
     def get_dataset(self, path : str = '/CDMS'): 
         # Get the childen in the path to check if a folder needs to be explored
@@ -28,6 +44,18 @@ class DCCrawler:
     def crawl(self, path : str = '/CDMS'):
         datasets = self.get_dataset(path)
 
+        fs_path = self.fs_prefix+self.dc_path
+        files = [os.path.join(dirpath, f) for (dirpath, dirnames, filenames) in os.walk(fs_path) for f in filenames]
+        files = [f[f.find(self.resource_prefix):] for f in files]
+
+        for dataset in datasets: 
+            for loc in dataset.locations: 
+                if loc.site == self.site: 
+                    print('Patching dataset.')
+                    #payload = {'scanStatus': 'OK'}
+                    #self.dc.client.patch_dataset(dataset.path, payload, site=self.site)
+                    # TODO: Add timestamp
+
 import argparse
 import tomli
 
@@ -35,26 +63,23 @@ def main(args : argparse.Namespace) -> None:
     '''
     '''
 
-    config = None
     dc_config  = None
     path = '/CDMS'
-    if args.config:
-        with open(args.config, 'rb') as f:
-            config = tomli.load(f)
+    
+    if not args.config: 
+        parser.error('A config file needs to be specified.')
+    
+    config = None
+    with open(args.config, 'rb') as f:
+        config = tomli.load(f)
 
-        if 'config' in config['catalog']: 
-            dc_config = config['catalog']['config']
-            print(dc_config)
-
-        if 'path' in config['crawler']:
-            path = config['crawler']['path']
-
+    if 'config' in config['catalog']: 
+        dc_config = config['catalog']['config']
     dc = CDMSDataCatalog(config_file=dc_config)
+    
+    crawler = DCCrawler(dc, config)
 
-    crawler = DCCrawler(dc)
-
-    crawler.crawl(path)
-
+    crawler.crawl()
 
 if __name__ == '__main__': 
 
